5ce04d1d1fd0f9c186d75716b3bcb20bb526c0ef
Adam <adammartin1913@gmail.com>
2019-08-06 09:56:36 -0400

    update

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,36 @@
 package hbcu.stay.ready.algor;
 
-public class Solution {
+import java.util.LinkedList;
 
-    public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+public class Solution {
+    String noteText = "this is a note for you from a secret admirer";
+
+    public boolean harmlessRansomNote(String meesage, String magazineText) {
+
+        String text = "puerto rico is a great place you must hike far from town to find a secret " +
+                "waterfall that i am an admirer of but note that it is not as hard as it seems " +
+                "this is my advice for you";
+
+        LinkedList<String> object = new LinkedList<String>();
+
+        object.add(text);
+
+        for (String x : object) {
+
+            if (x == noteText) {
+                
+
+            }
+
+            System.out.println(object);
+        }
+
+
+
+
+
+
+
+        return true;
     }
 }
